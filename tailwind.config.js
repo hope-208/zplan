8025dba1bf3130b2007eb5365ce8e468b41b545a
Nadezhda Telepenina <Telepenina.N.S@yandex.ru>
2025-02-05 18:59:57 +0400

    Add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./src/**/*.{html,js}', './src/*.{html,js,pug}'],
   theme: {
-    extend: {},
+    extend: {
+      '--color-gray-50': 'oklch(91.68% 0.0098 252.82)',
+      '--color-gray-200': 'oklch(92.76% 0.0058 264.53)',
+      '--color-emerald-400': 'oklch(72.46% 0.143 168.76)',
+      '--color-red-500': 'oklch(62.38% 0.2277 26.96)',
+    },
   },
   plugins: [],
 };
